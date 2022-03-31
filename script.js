ba15647940b6a5143f184844f8d4972306927f9f
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-03-31 15:13:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+    
+    document.querySelector('.theme-name').textContent = newTheme;
+  }
+  
+  document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+});
